2dec08d9f6a8208b1c5d3dc95eb2bad7034b062a
Xiaoting Lian <lian1995226@gmail.com>
2022-02-09 12:55:04 -0500

    started testing

---
+++ Guess a number/src/test/guessANumberTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+class guessANumberTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+}
