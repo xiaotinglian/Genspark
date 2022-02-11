6d3ddeb89a7531da63108d78bb8b80c4788cff0d
Xiaoting Lian <lian1995226@gmail.com>
2022-02-11 15:41:06 -0500

    started testing

--- Guess a number/src/guessnumber/guessANumber.java
+++ Guess a number/src/guessnumber/guessANumber.java
@@ -4,26 +4,51 @@ import java.util.Scanner;
 public class guessANumber {
 
     public static int setNum(){
-        System.out.println("Take a guess.");
-        Scanner getInteger = new Scanner(System.in);
-        int input = getInteger.nextInt();
-        while (input < 1 || input > 20) {
-            System.out.println("Wrong number, try numbers between 1 and 20.");
+        int input = 0;
+        try {
+            System.out.println("Take a guess.");
+            Scanner getInteger = new Scanner(System.in);
             input = getInteger.nextInt();
+            while (input < 1 || input > 20) {
+                System.out.println("Wrong number, try numbers between 1 and 20.");
+                input = getInteger.nextInt();
+            }
+            
+        }catch(Exception e){
+            System.out.println("exception in setNum");
         }
         return input;
     }
-    public static String setName(){
-        Scanner getInput = new Scanner(System.in);
-        return getInput.nextLine();
+    public static String setName() {
+        String name = null;
+        try {
+            Scanner getInput = new Scanner(System.in);
+            name = getInput.nextLine();
+        } catch (Exception o) {
+            System.out.println("exception in setName");
+        }
+        return name;
     }
     public static int setRand(){
-        Random rand = new Random();
-        return rand.nextInt(20);
+        int rando=0;
+        try {
+            Random rand = new Random();
+            rando= rand.nextInt(20);
+        }catch(Exception i){
+            System.out.println("Exception in set random number");
+        }
+        return rando;
     }
     public static char replay(){
-        Scanner choice = new Scanner(System.in);
-        return choice.next().charAt(0);
+        char again = 0;
+        System.out.println("Would you like to play again? (y/n)");
+        try {
+            Scanner choice = new Scanner(System.in);
+            again = choice.next().charAt(0);
+        }catch(Exception w){
+            System.out.println("exception in replay");
+        }
+        return again;
     }
 
 
@@ -47,13 +72,13 @@ public class guessANumber {
                 System.out.println("Your guess is too low");
             } else {
                 System.out.println("Good job, " + user + "! Your guessed my number in " + count + " guesses!");
-                System.out.println("Would you like to play again? (y/n)");
                 char play =replay();
+
                 if (play=='y') {
                     System.out.println("Well, " + user + ", I am thinking of a number between 1 and 20.");
                     randNum = setRand();
                     count = 0;
-                    time = 7;
+                    time = 6;
                 }
                 if (play=='n') {
                     break;
