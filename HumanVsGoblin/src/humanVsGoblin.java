b17515e93929f5cd98fc4cca3bc9b37371296b08
Xiaoting Lian <lian1995226@gmail.com>
2022-03-01 12:06:26 -0500

    humanVsGoblin some changes

--- HumanVsGoblin/src/humanVsGoblin.java
+++ HumanVsGoblin/src/humanVsGoblin.java
@@ -1,6 +1,9 @@
 public class humanVsGoblin {
-    public static Void main(String[] args){
-
+    public static void main(String[] args){
+        System.out.println("==========================================");
+        System.out.println("  Welcome to Human vs. Goblin Game  ");
+        System.out.println("==========================================");
+        System.out.println();
 
 
     }
