3c88006b70cac514875fc208346385c707df05b0
Xiaoting Lian <lian1995226@gmail.com>
2022-03-06 11:52:58 -0500

    humanVsGoblin some changes

--- HumanVsGoblin/src/HumanVsGoblin.java
+++
@@ -1,8 +0,0 @@
-public class HumanVsGoblin {
-    public static void main(String[] args){
-        System.out.println("==========================================");
-        System.out.println("  Welcome to Human vs. Goblin Game  ");
-        System.out.println("==========================================");
-
-    }
-}
--- HumanVsGoblin/src/Humanoid.java
+++
@@ -1,9 +0,0 @@
-public class Humanoid {
-    int x;
-    int y;
-
-    public Humanoid(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
